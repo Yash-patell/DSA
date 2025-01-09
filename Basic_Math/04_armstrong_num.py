a475412f884ffea8cb6050a526e40fefcca2be7e
Yash <yashpatel.ypyash@gmail.com>
2025-01-09 16:39:12 +0530

    armstrong + palindrome

---
+++ Basic_Math/04_armstrong_num.py
@@ -0,0 +1,20 @@
+num = int(input("enter num : "))
+
+abs_num = abs(num)
+stack = 0
+
+#count the number of digits in input
+count = len(str(num))
+
+while num>0:
+    last_digit = num % 10 #extracting the last digit
+    multiplied_val = last_digit * count  # exponentiation last digit by count
+    stack += multiplied_val #adding the exponetiated value
+    
+    num //=10 #removing the last digit
+    
+if abs_num == stack:
+    print("Armstrong number ")
+else: print(f"False - {abs_num} not a armstrong number")
+    
+    
