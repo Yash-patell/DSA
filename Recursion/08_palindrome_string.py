b9c5fc30bcce3eb57254a3e0885aff997fb4875a
Yash <yashpatel.ypyash@gmail.com>
2025-02-09 00:10:05 +0530

    9-2-25

---
+++ Recursion/08_palindrome_string.py
@@ -0,0 +1,21 @@
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+
+def is_palindrome(s):
+    
+    new_string = ''.join(c.lower() for c in s if c.isalnum())
+    
+    def palindorme_check(start, end):
+        
+        if start >= end:
+            
+            return True #base case if pointer has met or crossed it is a palindrome
+        
+        if new_string[start] != new_string[end]:
+            return False 
+        
+        return palindorme_check(start +1, end -1 )
+    
+    return palindorme_check(0, len(new_string)-1)      
+
+print(is_palindrome('lol'))      
