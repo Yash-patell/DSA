626041f52b1eed84c51036082dbf8489ae437293
Yash <yashpatel.ypyash@gmail.com>
2025-03-10 01:42:50 +0530

    revision of sorting

--- sorting/1_selection_sort.py
+++ sorting/1_selection_sort.py
@@ -1,7 +1,7 @@
 arr = [23,10,5,2,1,3,42]
 
 n = len(arr)
-for i in range (n):
+for i in range (0,n):
 
 #assume current element is min element    
     min_index = i
@@ -10,8 +10,10 @@ for i in range (n):
         
         if arr[j] < arr[min_index]: #checks if current element is smaller than the minimum element
             
+# if there is any element 'j' smaller than the current minimum element 'min_index' then make the j, minimum element            
             min_index = j #min element chnages from i to j
-            
+
+# i is the current position and min_index is the min element            
     arr[i],arr[min_index] = arr[min_index],arr[i]
         
 print(arr)
